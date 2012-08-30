9b788f07e33cf60e702748bd72d6b51b8cecfcff
Heather Arthur <fayearthur@gmail.com>
2012-08-30 16:47:50 +0100

    add hsl input field

--- scheme/test.js
+++ scheme/test.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
   $("#markup-code").html(markup);
 
   $(".color-swatch").click(function() {
-    console.log(this);
     var picker = new ui.ColorPicker;
 
     ui.dialog('Select a color', picker)
@@ -17,8 +16,8 @@ $(document).ready(function() {
       .show();
 
     var swatch = this;
-
-    var className = $(swatch).data("class");
+    var parent = $(swatch).parent();
+    var className = parent.data("class");
     var color = $("." + className).css("color");
 
     picker.hue(color);
@@ -28,6 +27,11 @@ $(document).ready(function() {
       $("." + className).css("color", color);
 
       $(swatch).css("background-color", color);
+
+      var hsl = Color(color).hslString();
+
+      var value = parent.children(".color-value");
+      value.val(hsl);
     });
   })
 });
