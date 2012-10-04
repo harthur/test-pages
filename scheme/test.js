08db4bab9668e2206050dd401db379599b55e21b
Heather Arthur <fayearthur@gmail.com>
2012-10-04 11:42:27 -0700

    add spectrum color picker, and js highlighting sample

--- scheme/test.js
+++ scheme/test.js
@@ -7,31 +7,56 @@ $(document).ready(function() {
 
   $("#markup-code").html(markup);
 
-  $(".color-swatch").click(function() {
-    var picker = new ui.ColorPicker;
-
-    ui.dialog('Select a color', picker)
-      .effect('slide')
-      .closable()
-      .show();
-
+  $(".color-swatch").each(function() {
     var swatch = this;
     var parent = $(swatch).parent();
     var className = parent.data("class");
     var color = $("." + className).css("color");
 
-    picker.hue(color);
-    picker.render();
+    $(this).spectrum({
+      preferredFormat: "rgb",
+      showInput: true,
+      localStorageKey: className,
+      change: function(color) {
+        var rgb = color.toRgbString();
+        setColor(className, rgb);
 
-    picker.on('change', function(color) {
-      $("." + className).css("color", color);
-
-      $(swatch).css("background-color", color);
-
-      var hsl = Color(color).hslString();
-
-      var value = parent.children(".color-value");
-      value.val(hsl);
+        var value = parent.children(".color-value");
+        value.val(rgb);
+      }
     });
-  })
+
+    var color = $(this).spectrum("get");
+    setColor(className, color);
+  });
+
+  $(".color-value").change(function() {
+    var val = $(this).val();
+    var parent = $(this).parent();
+    var className = parent.data("class");
+    var color = $("." + className).css("color");
+
+    var picker = parent.children(".color-swatch");
+
+    setColor(className, val);
+
+    picker.spectrum("set", val);
+  });
 });
+
+function setColor(className, color) {
+  console.log(className, color);
+  var styleSheet = document.styleSheets[3];
+
+  var selector = "." + className;
+  var rules = styleSheet.cssRules;
+
+  for (var i = 0; i < rules.length; i++) {
+    var rule = rules[i];
+    if (rule.selectorText == selector) {
+      styleSheet.deleteRule(i);
+    }
+  }
+
+  styleSheet.insertRule(selector + "{ color: " + color + " !important; }", 0);
+}
